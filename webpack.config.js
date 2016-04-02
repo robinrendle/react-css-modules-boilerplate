74c1ed6515c27918c59deddfb3478d2b3e8919b0
Robin Rendle <robinrendle@Robins-MacBook-Pro.local>
2016-04-02 21:14:11 +0100

    css-modules

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+module.exports = {
+    entry:  './src',
+    output: {
+        path: 'build',
+        filename: 'bundle.js',
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel',
+                include: __dirname + '/src',
+            },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract('css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!sass'),
+                include: __dirname + '/src'
+            }
+        ],
+    },
+    plugins: [
+        new ExtractTextPlugin("styles.css"),
+        new BrowserSyncPlugin({
+            host: 'localhost',
+            port: 3000,
+            server: { baseDir: ['./']}
+        })
+    ]
+};
