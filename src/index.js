2843f48291664bbc1969fdfba47884da9b8fea77
Robin Rendle <robinrendle@Robins-MacBook-Pro.local>
2016-04-15 14:22:02 +0100

    removed old index code

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import styles from './app.scss';
-
-var element = `
-    <div class="${styles.thing}">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur laudantium recusandae itaque libero velit minus ex reiciendis veniam. Eligendi modi sint delectus beatae nemo provident ratione maiores, voluptatibus a tempore!</p>
-    </div>
-`
-
-document.write(element);
